7473b364cd0ebd9f646af49c219e3de1ce1f1f9c
Chinavada Prasanna <166499599+chinavadaprasanna@users.noreply.github.com>
2025-04-22 11:49:56 +0530

    Update constants.js

--- chatbot/constants.js
+++ chatbot/constants.js
@@ -1,99 +1,104 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const inputField = document.getElementById("input");
-    inputField.addEventListener("keydown", (e) => {
-      if (e.code === "Enter" || e.keyCode===13) {
-        let input = inputField.value;
-        inputField.value = "";
-        output(input);
-      }
-      
-    });
-  });
+// Options the user could type in
+let date=new Date(); 
+var currentdate=new Date().toLocaleDateString();
+var time=new Date().toLocaleTimeString();
+const prompts = [
+    ["hi", "hey", "hello", "good morning", "good afternoon"],
+    ["how are you", "how is life", "how are things"],
+    ["what are you doing", "what is going on", "what is up"],
+    ["how old are you"],
+    ["who are you", "are you human", "are you bot", "are you human or bot"],
+    ["who created you", "who made you"],
+    [
+      "your name please",
+      "your name",
+      "may i know your name",
+      "what is your name",
+      "what call yourself"
+    ],
+    ["i love you"],
+    ["happy", "good", "fun", "wonderful", "fantastic", "cool"],
+    ["bad", "bored", "tired"],
+    ["help me", "tell me story", "tell me joke"],
+    ["ah", "yes", "ok", "okay", "nice"],
+    ["bye", "good bye", "goodbye", "see you later"],
+    ["what should i eat today"],
+    ["bro"],
+    ["what", "why", "how", "where", "when"],
+    ["no","not sure","maybe","no thanks"],
+    [""],
+    ["haha","ha","lol","hehe","funny","joke"],
+    ["food donate","project" ],
+    ["date"],
+    ["time"],
+    ["what can i donate","donate"],
+    ["trust in madurai","ngos in madurai"],
+    ["tell joke"],
+    ["how can I package my cooked or raw food donations","cooked food donation","raw food donate"],
+    ["how my donation is used"],
+    ["can i donate cooked food"],
+    ["what are the guidelines for donating"]
+  ]
   
-  function output(input) {
-    let product;
+  // Possible responses, in corresponding order
   
-    // Regex remove non word/space chars
-    // Trim trailing whitespce
-    // Remove digits - not sure if this is best
-    // But solves problem of entering something like 'hi1'
+  const replies = [
+    ["Hello!", "Hi!", "Hey!", "Hi there!","Howdy"],
+    
+    [
+      "Fine... how are you?",
+      "Pretty well, how are you?",
+      "Fantastic, how are you?"
+    ],
+    [
+      "Nothing much",
+      "About to go to sleep",
+      "Can you guess?",
+      "I don't know actually"
+    ],
+    ["I am infinite"],
+    ["I am just a bot", "I am a bot. What are you?"],
+    
+    ["kishor and uppili"],
+    ["I am chitti"],
+    ["I love you too", "Me too"],
+    ["Have you ever felt bad?", "Glad to hear it"],
+    ["Why?", "Why? You shouldn't!", "Try watching TV"],
+    ["What about?", "Once upon a time..."],
+    ["Tell me a story", "Tell me a joke", "Tell me about yourself"],
+    // ["varata mame durr"],
+    ["Bye", "Goodbye", "See you later"],
+    ["Sushi", "Pizza"],
+    ["Bro!"],
+    ["Great question"],
+    ["That's ok","I understand","What do you want to talk about?"],
+    ["Please say something :("],
+    ["Haha!","Good one!"],
+    ["  The basic concept of this project  Food Waste Management is to collect theexcess/leftover food from donors such as hotels, restaurants, marriage halls, etc and distribute to  the  needy people"],
+    [currentdate],
+    [time],
+    ["you can donate raw ,cooked and packed foods"],
+    ["Madurai old age Charitable Trust,208, East Veli Street, Near Keshavan Hospital, "],
+    ["joke ha ha .."],
+    ["You can package your cooked or raw food donations in airtight containers such as Tupperware or plastic bags. You can also use aluminum foil or cling wrap to keep the food fresh. Please make sure to label the containers with the type of food, date, and any relevant instructions"],
+     ["Your donation will be used to support our mission and the various programs and initiatives that we have in place. Your donation will help us to continue providing assistance and support to those in need. You can find more information about our programs and initiatives on our website"],
+     ["Yes, you can donate cooked food as long as it is prepared in a licensed kitchen, packaged properly and kept at safe temperatures. Please contact us for further instructions and guidelines"],
+    ["When donating raw ingredients, please ensure that the items are unopened and unexpired."]
+  ]
+
   
-    let text = input.toLowerCase().replace(/[^\w\s]/gi, "").replace(/[\d]/gi, "").trim();
-    text = text
-      .replace(/ a /g, " ")   // 'tell me a story' -> 'tell me story'
-      .replace(/i feel /g, "")
-      .replace(/whats/g, "what is")
-      .replace(/please /g, "")
-      .replace(/ please/g, "")
-      .replace(/r u/g, "are you")
-      .replace(/tell about /g,"");
+  // Random for any other user input
   
-    if (compare(prompts, replies, text)) { 
-      // Search for exact match in `prompts`
-      product = compare(prompts, replies, text);
-    } else if (text.match(/thank/gi)) {
-      product = "You're welcome!"
-    } else if (text.match(/(corona|covid|virus)/gi)) {
-      // If no match, check if message contains `coronavirus`
-      product = coronavirus[Math.floor(Math.random() * coronavirus.length)];
-    } else {
-      // If all else fails: random alternative
-      product = alternative[Math.floor(Math.random() * alternative.length)];
-    }
+  const alternative = [
+    // "Same",
+    // "Go on...",
+    // "Bro...  i don't know",
+    // "Try again",
+    // // "I'm listening...:/",
+    // "I don't understand ",
+    " 😢sorry i am still under development "
+  ]
   
-    // Update DOM
-    addChat(input, product);
-  }
+  // Whatever else you want :)
   
-  function compare(promptsArray, repliesArray, string) {
-    let reply;
-    let replyFound = false;
-    for (let x = 0; x < promptsArray.length; x++) {
-      for (let y = 0; y < promptsArray[x].length; y++) {
-        if (promptsArray[x][y] === string) {
-          let replies = repliesArray[x];
-          reply = replies[Math.floor(Math.random() * replies.length)];
-          replyFound = true;
-          // Stop inner loop when input value matches prompts
-          break;
-        }
-      }
-      if (replyFound) {
-        // Stop outer loop when reply is found instead of interating through the entire array
-        break;
-      }
-    }
-    return reply;
-  }
-  
-  function addChat(input, product) {
-    const messagesContainer = document.getElementById("messages");
-  
-    let userDiv = document.createElement("div");
-    userDiv.id = "user";
-    userDiv.className = "user response";
-    userDiv.innerHTML = `<img src="img/user.png" class="avatar"><span>${input}</span>`;
-    messagesContainer.appendChild(userDiv);
-  
-    let botDiv = document.createElement("div");
-    let botImg = document.createElement("img");
-    let botText = document.createElement("span");
-    botDiv.id = "bot";
-    botImg.src = "img/bot-mini.png";
-    botImg.className = "avatar";
-    botDiv.className = "bot response";
-    botText.innerText = "Typing...";
-    botDiv.appendChild(botText);
-    botDiv.appendChild(botImg);
-    messagesContainer.appendChild(botDiv);
-    // Keep messages at most recent
-    messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
-  
-    // Fake delay to seem "real"
-    setTimeout(() => {
-      botText.innerText = `${product}`;
-      textToSpeech(product)
-    }, 2000
-    )
-  
-  }
+  const coronavirus = ["Please stay home", "Wear a mask", "Fortunately, I don't have COVID", "These are uncertain times"]
